b7b87e77268d09b901884fad4de34a2fe4227cee
gwarz <gmwarzecha@tutanota.com>
2021-05-07 14:43:00 -0700

    added typeDefs, some still need some info added

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -4,7 +4,35 @@ const { gql } = require('apollo-server-express');
 // creates typeDefs
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, password: String!): Auth
+    saveBook()
+    removeBook()
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [Author]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
